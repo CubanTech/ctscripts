bb8e4a9ffec446613282c12a887d165536330755
Olemis Lang <olemis+work@gmail.com>
2017-10-15 00:22:56 -0400

    [papercall] - Transform Papercall submissions.json onto speaker profile info

---
+++ jsont/papercall.authors.t.js
@@ -0,0 +1,4 @@
+{
+  "self": "[\n  {$}\n]",
+  "self[*]" : "\{\n    \"name\" : \"{$.name}\",\n    \"avatar\" : \"{$.avatar}\",\n    \"bio\" : \"{$.bio}\",\n    \"organaization\" : \"{$.organization}\"\n  \}",
+}
